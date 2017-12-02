88d5b087774aec8c4d3ca34bdabac62c055b7b70
franky_OnePiece <soufianecompaq@gmail.com>
2017-12-02 01:31:14 +0000

    first upload of the project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController , Platform} from 'ionic-angular';
+import { CallNumber } from '@ionic-native/call-number';
+import { Contacts } from '@ionic-native/contacts';
+
+
+
+/*import { CallsPage } from '../calls/calls';
+import { ContactsPage } from '../contacts/contacts';
+import { MessagesPage } from '../messages/messages';*/
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [Contacts]
 })
 export class HomePage {
+  pet: string="call";
 
-  constructor(public navCtrl: NavController) {
+  phoneNbr : number;
 
+  ContactItems:any;
+
+  constructor( private contacts: Contacts, private call : CallNumber, public platform:Platform ,public navCtrl: NavController) {
+
+    this.platform.ready().then(()=>{
+      this.contacts.find(["*"]).then((contacts) =>{
+        this.ContactItems=contacts;
+      },(error) =>{
+        console.log(error)
+      });
+    })
   }
-
+  async callnbr():Promise<any>{
+    try{
+      await this.call.callNumber(String(this.phoneNbr),true);
+    }catch(e){
+      console.error(e);
+    }
+  }
+  
 }
